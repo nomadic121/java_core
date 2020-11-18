07de0253d2826b5e67383c868c92cb847e21c1e0
rgazizov <rgazizov@i-novus.ru>
2020-11-18 20:14:08 +0300

    lesson 02

---
+++ Lesson_02/task_05.js
@@ -0,0 +1,10 @@
+// 5. Завести три переменных start, end, point. Вывести входит ли point в диапазон от start до end
+
+'use strict'
+
+let start = 0;
+let end = 10;
+let point = 1;
+
+if (point >= start && point <= end) console.log("point входит в диапазон")
+else console.log("point не входит в диапазон")
